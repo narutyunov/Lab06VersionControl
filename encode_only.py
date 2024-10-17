ae94e3676fe132c33da8b4ae8d48b8aa6554d1aa
narutyunov <narutyunov@ufl.edu>
2024-10-17 19:24:48 -0400

    Fixed spacing not matching example output and added a period to the output of option 2 to match the example output better

--- encode_only.py
+++ encode_only.py
@@ -25,16 +25,16 @@ if __name__ == "__main__":
         print("-------------")
         print("1. Encode")
         print("2. Decode")
-        print("3. Quit")
+        print("3. Quit\n")
 
         option = int(input("Please enter an option: "))
 
         if option == 1:
             password = input("Please enter your password to encode: ")
             stored_password = encode(password)
-            print("Your password has been encoded and stored!")
+            print("Your password has been encoded and stored!\n")
         elif option == 2:
-            print(f"The encoded password is {stored_password}, and the original password is {decode(stored_password)}")
+            print(f"The encoded password is {stored_password}, and the original password is {decode(stored_password)}.\n")
         elif option == 3:
             exit()
 
